3a1364f49bc3c892833fe49d8a3f08da019acc95
Edge <Androedge@gmail.com>
2020-09-03 02:38:05 +0300

    eugene JwtTokenProvider

--- src/test/java/ru/autoins/oto_registry_rest/OtoRegistryRestApplicationTests.java
+++ src/test/java/ru/autoins/oto_registry_rest/OtoRegistryRestApplicationTests.java
@@ -3,11 +3,22 @@ package ru.autoins.oto_registry_rest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Instant;
+import java.util.Date;
+
 @SpringBootTest
 class OtoRegistryRestApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
+//    @Test
+//    public void test() {
+//        System.out.println("Sys: " + System.currentTimeMillis());
+//        System.out.println("Instant: " + Instant.now().toEpochMilli());
+//        System.out.println("Date: " + new Date().toInstant().toEpochMilli());
+//        System.out.println("Date2: " + new Date().getTime());
+//    }
 
 }
